5976ec1cb853831e7a93537356e3ce88f7871944
Mux <muxxum65536@gmail.com>
2020-12-13 17:07:45 +0900

    0x01 done, 0x02 not yet

---
+++ 0x01/0x01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Solution {
+    public int[] TwoSum (int[] nums, int target) {
+        System.Collections.Generic.Dictionary<int, int> map = new System.Collections.Generic.Dictionary<int, int> ();
+        for (int i = 0; i < nums.Length; ++i) {
+            if (map.ContainsKey (target - nums[i])) {
+                return new int[] { map[target - nums[i]], i };
+            }
+            map[nums[i]] = i;
+        }
+        return new int[0];
+    }
+
+    public static void Main () {
+        Solution s = new Solution ();
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        int[] res = s.TwoSum (nums, target);
+
+        for (int i = 0; i < res.Length; ++i) {
+            Console.WriteLine (res[i]);
+        }
+    }
+}
---
+++ 0x02/0x02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode (int val = 0, ListNode next = null) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
+public class Solution {
+    public ListNode AddTwoNumbers (ListNode l1, ListNode l2) {
+        ListNode dummy_head = new ListNode ();
+        bool is_shift = false;
+        ListNode cur = dummy_head.next;
+        while (l1 != null && l2 != null) {
+            if(is_shift)
+                int v = l1.val  + ls2.val + 1;
+            if(cur == null){
+                cur = new ListNode(v);
+            }else{
+                cur.val = v;
+            }
+
+            cur = cur.next; l1 = l1.next; l2=l2.next;
+
+        }
+    }
+}
