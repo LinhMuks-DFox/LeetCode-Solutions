12d1203f9ab793a5ad89b7346301beb22023c854
Mux <muxxum65536@gmail.com>
2020-12-14 13:45:34 +0900

    0x0D done, 0x09Not yet

---
+++ 0x09/0x09/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _0x09 {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+
+    public class Solution {
+        public bool IsPalindrome (int x) {
+            if (x < 0 || (x % 10 == 0 && x != 0)) {
+                return false;
+            }
+            int cur = 0;
+            while (x != cur) {
+                revertedNumber = revertedNumber * 10 + x % 10;
+                x /= 10;
+            }
+
+            return x == cur || x == cur / 10;
+        }
+    }
+}
---
+++ 0x0D/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _0x0D {
+    class Program {
+        static void Main (string[] args) {
+            Solution s = new Solution ();
+
+            Console.WriteLine ($"VIIV: {s.RomanToInt("VIIV")}");
+        }
+    }
+
+    public class Solution {
+
+        public int RomanToInt (string s) {
+            int sum = 0;
+            int preNum = Map (s[0]);
+            for (int i = 1; i < s.Length; i++) {
+                int num = Map (s[i]);
+                if (preNum < num) {
+                    sum -= preNum;
+                } else {
+                    sum += preNum;
+                }
+                preNum = num;
+            }
+            sum += preNum;
+            return sum;
+        }
+
+        private int Map (char c) {
+            switch (c) {
+                case 'I':
+                case 'i':
+                    return 1;
+                case 'V':
+                case 'v':
+                    return 5;
+                case 'X':
+                case 'x':
+                    return 10;
+                case 'L':
+                case 'l':
+                    return 50;
+                case 'C':
+                case 'c':
+                    return 100;
+                case 'D':
+                case 'd':
+                    return 500;
+                case 'M':
+                case 'm':
+                    return 1000;
+                default:
+                    return default;
+            }
+        }
+    }
+}
