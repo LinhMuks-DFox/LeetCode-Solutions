f924edb5b5dd152d44b245653f4f77455073ca1f
Mux <muxxum65536@gmail.com>
2020-12-19 17:01:43 +0900

    0x08 done; not tested yet

---
+++ 0x08/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+/*
+    Link: https://leetcode-cn.com/problems/string-to-integer-atoi/
+    
+    1. Normal case
+    >>>"42"
+    -->42
+
+    2. Normal case2
+    >>>"   42"
+    -->42
+    
+    3. Return 0 while the first non-null char is not a number.
+    >>>"This is test of convert string'42' to int"
+    -->0
+
+    4. Return (2**31-1 / -2**31) when inputted number is overflow
+    >>>"-91283472332" 
+    >>>-2147483648
+*/
+public class Solution
+{
+    static void Main(string[] args)
+    {
+        string t = "-91283472332";
+        Console.WriteLine(MyAtoi(t));
+    }
+
+    public static int MyAtoi(string s)
+    {
+        Converter converter = new Converter();
+        return converter.Convert(s);
+    }
+}
+
+public class Converter
+{
+    public int Convert(string str)
+    {
+        int n = str.Length, index = 0;
+        int sign = 1; int ret = 0;
+        while (index < n && str[index] == ' ')
+        {
+            index++;
+        }
+        if (index == n)
+            return 0;
+        if (str[index] == '+')
+        {
+            index++;
+        }
+        else if (str[index] == '-')
+        {
+            sign *= -1;
+        }
+        else if (!IsDigit(str[index]))
+            return 0;
+        while (index < n)
+        {
+            if (!IsDigit(str[index]))
+                return 0;
+            int digit = str[index] - '0';
+            if (ret > (Int32.MaxValue - digit) / 10) {
+                // 本来应该是 ans * 10 + digit > Integer.MAX_VALUE
+                // 但是 *10 和 + digit 都有可能越界，所有都移动到右边去就可以了。
+                return sign==-1? Int32.MinValue : Int32.MaxValue;
+            }
+            ret = ret * 10 + digit;
+            index++;
+        }
+
+        return ret;
+    }
+
+    private bool IsDigit(char c)
+    {
+        return ('0' <= c && c <= '9');
+    }
+}
+
+
+
