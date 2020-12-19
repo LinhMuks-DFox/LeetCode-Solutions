fd0dd99fa9123463887ffb02889d4133d3973ba4
Mux <muxxum65536@gmail.com>
2020-12-19 17:50:44 +0900

    0x14 done

---
+++ 0x14/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+    Link: https://leetcode-cn.com/problems/valid-parentheses/
+*/
+namespace _0x14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().IsValid("(){}}{"));
+        }
+    }
+    /*Tested! Done! but not fast!*/
+    class Solution
+    {
+        public bool IsValid(string s)
+        {
+            if (s.Length % 2 == 1)
+                return false;
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> map = new Dictionary<char, char>();
+            map['}'] = '{'; map[')'] = '('; map[']'] = '[';
+            foreach (char cur in s)
+            {
+                if (map.ContainsKey(cur))
+                {
+                    if (stack.Count == 0 || stack.Pop() != map[cur])
+                        return false;
+                }
+                else
+                    stack.Push(cur);
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
