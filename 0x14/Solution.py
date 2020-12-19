ae72c9175d0839b64ca74221dc65981a35f78e7d
Mux <muxxum65536@gmail.com>
2020-12-19 18:02:32 +0900

    0x14 Solution python version added

---
+++ 0x14/Solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def is_valid(self, s):
+        stack = []
+        if len(s) % 2 == 1:
+            return False
+        for i in s:
+            if i == "(" or i == "[" or i == "{":
+                stack.append(i)
+            else:
+                if len(stack) == 0:
+                    return False
+                ch = stack.pop()
+                if i == ")" and ch != "(":
+                    return False
+                if i == "]" and ch != "[":
+                    return False
+                if i == "}" and ch != "{":
+                    return False
+        return len(stack) == 0
