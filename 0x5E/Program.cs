16f07a3f55adffc9ebbb15bc882ce0b53861a512
Mux <muxum65536@gmail.com>
2021-01-21 17:31:22 +0900

    No.94 added, done

---
+++ 0x5E/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+    Link: https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+*/
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) 
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+     }
+}
+
+public class Solution{
+
+    public IList<int> InorderTraversal(TreeNode root) {
+        List<int> ret = new List<ret>(10);
+        InorderTraversal(root, ret);
+        return ret;
+    }
+
+    public void InorderTraversal(TreeNode node, IList<int> list){
+        if(node == null)
+            return;
+        InorderTraversal(node.left, list);
+        lsit.Add(node.val);
+        InorderTraversal(node.right, list);
+    }
+}
