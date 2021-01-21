c39869d1ee28efd27db8f0478831e43d7f2ba4e9
Mux <muxum65536@gmail.com>
2021-01-21 17:24:52 +0900

    144(0x90) soluted

--- 0x90/Program.cs
+++ 0x90/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+/*
+    Link: https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+*/
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
-namespace _0x90
-{
-    class Program
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) 
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        this.val = val;
+        this.left = left;
+        this.right = right;
+     }
+}
+class Solution
+{
+    public IList<int> PreorderTraversal(TreeNode root) 
+    {
+        List<int> ret = new List<int>(10);
+        PreorderTraversal(root, ret);
+        return ret;
+    }
+
+    public void PreorderTraversal(TreeNode node, List<int> list)
+    {
+        if(node == null)
+            return;
+        list.Add(node.val);
+        PreorderTraversal(node.left, list);
+        PreorderTraversal(node.right, list);
     }
 }
+
+
