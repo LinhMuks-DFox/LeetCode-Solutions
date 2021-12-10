9e8aeed775f6afce283fec8d91c0aa5ca498de02
dotFoxyyyy <muxxum65536@gmail.com>
2021-12-10 23:40:35 +0900

    doing 0x02

--- 0x02/Program.cs
+++ 0x02/Program.cs
@@ -12,31 +12,42 @@
  * }
  */
 
-public class ListNode {
+public class ListNode
+{
     public int val;
     public ListNode next;
-    public ListNode (int val = 0, ListNode next = null) {
+    public ListNode(int val = 0, ListNode next = null)
+    {
         this.val = val;
         this.next = next;
     }
 
 }
-public class Solution {
-    public ListNode AddTwoNumbers (ListNode l1, ListNode l2) {
-        ListNode dummy_head = new ListNode ();
-        bool is_shift = false;
-        ListNode cur = dummy_head.next;
-        while (l1 != null && l2 != null) {
-            if(is_shift)
-                int v = l1.val  + ls2.val + 1;
-            if(cur == null){
-                cur = new ListNode(v);
-            }else{
-                cur.val = v;
+public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode ret_dummy_head = new ListNode();
+        int carry;
+
+        while (l1 != null || l2 != null)
+        {
+            int n1 = l1 == null ? 0 : l1.val,
+                n2 = l2 == null ? 0 : l2.val;
+
+            if (l1 == null)
+            {
+
             }
 
-            cur = cur.next; l1 = l1.next; l2=l2.next;
+            if (l2 == null) 
+            {
 
+            }
+
+            l1 = l1.next; l2 = l2.next;
         }
+
+        return ret_dummy_head.next;
     }
 }
