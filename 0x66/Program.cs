1a056afe6661f47df75e1ee78cdbc0bc0302474e
dotFoxyyyy <muxxum65536@gmail.com>
2021-12-30 00:01:59 +0900

    0x5E solving

---
+++ 0x66/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) 
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+     }
+}
+
+public class Solution{
+    public IList<IList<int>> LevelOrder(TreeNode root) {
+        Queue<TreeNode> q = new Queue<TreeNode>();
+        List<int> ret = new List<int>();
+        while(q.Count != 0){
+            TreeNode cur = q.Dequeue();
+            ret.Add(cur.val);
+
+            if(cur.left != null) q.Enqueue(cur.left);
+            if(cur.right != null) q.Enqueue(cur.right);
+        }
+
+        return ret;
+    }
+}
